60b12944dd125d899c26bf7aa8154afeac9d955d
coder-sid852 <96578178+coder-sid852@users.noreply.github.com>
2022-02-27 19:45:16 +0530

    Add files via upload

---
+++ Feb24/test1.java
@@ -0,0 +1,15 @@
+package Feb24;
+
+public class test1 {
+    public static void talk() {
+        System.out.println("hello there!");
+    }
+
+    public static void talk(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        talk("Matthew");
+    }
+}
